5f45ab943d1df4adfd96576237d76ba792e671b5
federico <romerof14@gmail.com>
2022-02-19 14:46:47 -0300

    Homework de la unidad extra completado (ya testeado)

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -131,9 +131,9 @@ function sortArray(arr) {
   //Escribe tu código aquí
   arr.sort((elem1, elem2) => {
     if (elem1.length < elem2.length)
-      return 1;
-    else if (elem1.length > elem2.length)
       return -1;
+    else if (elem1.length > elem2.length)
+      return 1;
     else
       return 0;
   });
